2b4ca2d5b5365eca635fe61c021a6f3a763ad758
iconemey <iconenemy@gmail.com>
2025-05-22 11:57:35 +0300

    refactor(layout): remove redundant image dimensions from metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,20 +22,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Zwami",
     type: "website",
-    locale: "en_US",
     description:
       "We connect Ukrainian citizens living outside Ukraine with trusted, multilingual lawyers who specialize in immigration, asylum, family, and civil law.",
     siteName: "Zwami",
-    url: "https://zwami.online/",
-    images: [
-      {
-        url: "https://zwami.online/zwami-graph.png",
-        width: 1920,
-        height: 1080,
-        alt: "Zwami logo, featuring a blue with yellow accent color, displayed on a solid white background.",
-        type: "image/png",
-      },
-    ],
+    url: "https://zwami.online",
+    images: "https://zwami.online/zwami-graph.png",
   },
 };
 
